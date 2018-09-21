f6cc254980884ac9136c73b64047298eff9238e0
yitengzhao <yitengzhao@outlook.com>
2018-09-21 22:27:55 +0800

    栈第二次修改

--- 链栈.cpp
+++ 链栈.cpp
@@ -1,47 +1,78 @@
 #include<iostream>
 using namespace std;
 
+
 typedef struct StackNode
 {
 	int data;
 	struct StackNode *next;
- }StackNode,*LinkStack;
+}StackNode,*LinkStack;
 
-//初始化
-bool InitStack(LinkStack &S)
+bool init(LinkStack &S)
 {
 	S = NULL;
-	return true;
+	return 1;
 }
 
-//入栈
-bool Push(LinkStack &S, int e)
+bool Push(LinkStack &S,int e)
 {
 	LinkStack p = new StackNode;
 	p->data = e;
 	p->next = S;
 	S = p;
-	return true;
+	return 1;
 }
 
-//出栈
-bool Push(LinkStack &S, int &e)
+bool Pop(LinkStack &S, int &e)
 {
 	if (S == NULL)
-		return false;
+		return 0;
 	e = S->data;
 	LinkStack p = S;
 	S = S->next;
 	delete p;
-	return true;
-
+	return 1;
 }
 
-//取栈顶元素
-bool GetTop(LinkStack S, int &e)
+bool GetTop(LinkStack &S, int &e)
 {
 	if (S == NULL)
-		return false;
+		return 0;
 	e = S->data;
-	return true;
+	return 1;
+}
+
+bool Creat(LinkStack &S)
+{
+	S = NULL;
+	int n;
+	cout << "请输入入栈元素的个数：";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		LinkStack p = new StackNode;
+		cin >> p->data;
+		p->next = S;
+		S = p;
+	}
+	return 1;
+}
+
+bool Show(LinkStack &S)
+{
+	LinkStack p = S;
+	while (p != NULL)
+	{
+		cout << p->data << " ";
+		p = p->next;
+	}
+	return 1;
+}
+
+
+int main()
+{
+	LinkStack S;
+	Creat(S);
+	Show(S);
 }
--- 顺序栈.cpp
+++ 顺序栈.cpp
@@ -1,54 +1,76 @@
 #include<iostream>
+#include<cstdlib>
+#include<cmath>
 using namespace std;
 #define MAXSIZE 100
-#define OK 1
-#define ERROR 0
 
-typedef struct 
+
+typedef struct
 {
 	int *base;
 	int *top;
 	int stacksize;
 }SqStack;
 
-//初始化
-int InitStack(SqStack &S)
+bool Init(SqStack &S)
 {
 	S.base = new int[MAXSIZE];
 	if (!S.base)
 		exit(OVERFLOW);
 	S.top = S.base;
 	S.stacksize = MAXSIZE;
-	return OK;
+	return 1;
 }
 
-//入栈
-int Push(SqStack &S, int e)
+bool Push(SqStack &S, int e)
 {
 	if (S.top - S.base == S.stacksize)
-		return ERROR;
+		return 0;
 	*S.top++ = e;
-	return OK;
+	return 1;
 }
 
-//出栈
-int Pop(SqStack &S, int &e)
+bool Pop(SqStack &S, int &e)
 {
 	if (S.top == S.base)
-		return ERROR;
+		return 0;
 	e = *--S.top;
-	return OK;
+	return 1;
 }
 
-int GetTop(SqStack S)
+bool GetTop(SqStack S, int &e)
 {
-	if (S.top != S.base)
-		return *(S.top - 1);
+	if (S.top == S.base)
+		return 0;
+	e = *(S.top - 1);
+	return 1;
+}
+
+bool Show(SqStack S)
+{
+	if (S.base == S.top)
+		return 0;
+	cout << "输出：";
+	while (S.base != S.top)
+	{
+		cout << *--S.top<<" ";
+	}
+	return 1;
 }
 
 int main()
 {
-/*
-*/
+	SqStack S;
+	Init(S);
+	int n;
+	cout << "请输入入栈的个数：";
+	cin >> n;
+	int e;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> e;
+		Push(S, e);
+	}
+	Show(S);
 	return 0;
 }
